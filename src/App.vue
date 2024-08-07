abe534e5d3d3758c117f4a4c7d0ef698859f2897
GZF <13038085710g@gmail.com>
2024-08-07 08:45:03 +0800

    代码优化

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,8 @@
+<template>
+  <div style="width: 100px;height: 100px;background-color: yellow;"></div>
+</template>
+
 <script setup lang="ts">
 
 </script>
-
-<template>
-  <div style="width: 100px;height: 100px;background-color: yellow;">1231231</div>
-</template>
-
-<style scoped>
-</style>
+<style scoped></style>
